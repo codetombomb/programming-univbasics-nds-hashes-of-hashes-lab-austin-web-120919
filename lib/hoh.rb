c45a5bd5369b1532b2c4a899d27d418029ff7684
Tomas Tobar <codetombomb@gmail.com>
2019-12-04 05:18:06 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,8 +49,9 @@ def naming_system
   sub_category: {
     label: "Phylum",
     sub_category: {
-      label: "Class",
-      label: "Order",
+      label: "Class"{
+      label: "Order"
+      }
     }
   }
 }
